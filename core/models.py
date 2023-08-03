58aac805c352ce70f605bab781e33fd0a31c771f
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2023-08-03 12:11:20 +0600

    update core/models.py

--- core/models.py
+++ core/models.py
@@ -34,6 +34,9 @@ class Transaction(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     user_transactions = db.relationship('User', backref=db.backref('transactions', lazy=True))
 
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    product_transactions = db.relationship('Product', backref=db.backref('prod_transactions', lazy=True))
+
     def __repr__(self):
         return "<Transaction %r>" % self.transaction_id
 
@@ -42,6 +45,7 @@ class Transaction(db.Model):
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text, nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
